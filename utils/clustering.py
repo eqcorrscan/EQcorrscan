ed7d70e6d0286643d4793afd16519da737695537
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2015-07-13 05:03:44 +0000

    Add SVD in Alpha, currently not working as expected, possible indexing issue
    Former-commit-id: c4486921fd07e8bdef389ebd6961d938ae1cbc3c [formerly 5a24a378cbedcfd3ff7599ed77a141d732d39d28]
    Former-commit-id: ccbb85e3250afda460736b9681933fb733b24de0

--- utils/clustering.py
+++ utils/clustering.py
@@ -63,7 +63,7 @@ def distance_matrix(templates):
             dist_mat[i,j]=dist_mat.T[i,j]
     return dist_mat
 
-def cluster(templates):
+def cluster(templates, show=True):
     """
     Function to take a set of templates and cluster them, will return clustered
     templates
@@ -74,11 +74,158 @@ def cluster(templates):
                 each number relating to a cluster
     """
     from scipy.spatial.distance import squareform
-    from scipy.cluster.hierarchy import linkage
-    from scipy.cluster.hierachy import dendrogram
+    from scipy.cluster.hierarchy import linkage, dendrogram
+    import matplotlib.pyplot as plt
     dist_mat=distance_matrix(templates)
     dist_vec=squareform(dist_mat)
     # plt.matshow(dist_mat, aspect='auto', origin='lower', cmap=pylab.cm.YlGnB)
     Z = linkage(dist_vec)
     D = dendrogram(Z)
-    plt.show()
+    if show:
+        plt.show()
+    return Z
+
+def group_delays(templates):
+    """
+    Function to group template waveforms according to their delays
+
+    :type templates: List of obspy.Stream
+    :param templates: List of the waveforms you want to group
+
+    :returns: List of List of obspy.Streams where each initial list is a group
+            with the same delays
+    """
+    groups=[]
+    group_delays=[]
+    group_chans=[]
+    for i in xrange(1,len(templates)):
+        print 'Working on waveform '+str(i)+' of '+str(len(templates))
+        # Calculate the delays
+        starttimes=[]
+        chans=[]
+        for tr in templates[i]:
+            starttimes.append(tr.stats.starttime)
+            chans.append((tr.stats.station, tr.stats.channel))
+        delays=[starttimes[m]-min(starttimes) for m in xrange(len(starttimes))]
+        if len(groups)==0:
+            groups.append([templates[i]])
+            group_delays.append(delays)
+            group_chans.append(chans)
+        else:
+            j=0
+            match=False
+            while not match:
+                kmatch=0
+                for k in xrange(len(chans)):
+                    # Check if the channel and delay match another group
+                    if chans[k] in group_chans[j] and \
+                       delays[k]==group_delays[j][group_chans[j].index(chans[k])]:
+                        kmatch+=1 # increase the match index
+                if kmatch==len(chans): # If all the channels match, add it to the group
+                    groups[j].append(templates[i])
+                    match=True
+                elif j<len(groups)-1:
+                    j+=1
+                else:
+                    # Create a new group and break the loop
+                    groups.append([templates[i]])
+                    group_delays.append(delays)
+                    group_chans.append(chans)
+                    match=True # Use this to break the loop
+    return groups
+
+# def generate_families(templates):
+    # """
+    # Function to take a load of templates, group them according to their
+    # delays, then within these groups group them according to similarity
+    # and stack the results into tempates
+
+    # :type templates: List of obspy.Stream
+    # :param templates: List of all possible templates to use
+
+    # :returns: List of templates (stacked)
+    # """
+    # import stacking
+    # groups=group_delays(templates)
+    # for group in groups:
+        # Z=cluster(group, show=False)
+    # #### UNFINISHED!!!
+    # return stacked_templates
+
+def SVD_tetsing(templates):
+    """
+    Function to compute the SVD of a number of templates and return the singular
+    vectors and singular values of the templates.
+
+    :type templates: List of Obspy.Stream
+    :param templates: List of the templates to be analysed
+
+    :return: SVector(ndarray), SValues(ndarray) for each channel, stachans, List
+            of String (station.channel)
+
+    .. rubric:: Note
+
+    **IN ALPHA, not working as expected**
+
+    It is recommended that you align the data before computing the SVD, .e.g.,
+    the P-arrival on all templates for the same channel should appear at the same
+    time in the trace.
+    """
+    import numpy as np
+    # Convert templates into ndarrays for each channel
+    # First find all unique channels:
+    stachans=[]
+    for template in templates:
+        for tr in template:
+            stachans.append(tr.stats.station+'.'+tr.stats.channel)
+    stachans=list(set(stachans))
+    # Initialize a list for the output matrices, one matrix per-channel
+    SValues=[]
+    SVectors=[]
+    for stachan in stachans:
+        chan_mat=[templates[i].select(station=stachan.split('.')[0], \
+                                  channel=stachan.split('.')[1])[0].data \
+                  for i in xrange(len(templates)) if \
+                  len(templates[i].select(station=stachan.split('.')[0], \
+                                  channel=stachan.split('.')[1])) != 0]
+        chan_mat=[chan_mat[i]/np.max(chan_mat[i]) for i in xrange(len(chan_mat))]
+        chan_mat=np.asarray(chan_mat).T
+        print chan_mat.shape
+        print stachan
+        U, s, V = np.linalg.svd(chan_mat, full_matrices=False)
+        SValues.append(s)
+        SVectors.append(U.T)
+    return SVectors, SValues, stachans
+
+def SVD_2_stream_testing(SVectors, stachans, k, sampling_rate):
+    """
+    Function to convert the singular vectors output by SVD to streams, one for
+    each singular vector level, for all channels.
+
+    :type SVectors: ndarray
+    :param SVectors: Singular vectors
+    :type stachans: List of Strings
+    :param stachans: List of station.channel Strings
+    :type k: Int
+    :param k: Number of streams to return = number of SV's to include
+    :sampling_rate: Float
+
+    :returns: SVstreams, List of Obspy.Stream, with SVStreams[0] being
+            composed of the highest rank singular vectors.
+
+    .. rubric:: Note
+
+    **IN ALPHA, not working as expected**
+    """
+    from obspy import Stream, Trace
+    SVstreams=[]
+    for i in xrange(k):
+        SVstream=[]
+        for j in xrange(len(stachans)):
+            if len(SVectors[i]) > j:
+                SVstream.append(Trace(SVectors[i][j], \
+                                        header={'station': stachans[j].split('.')[0],
+                                                'channel': stachans[j].split('.')[1],
+                                                'sampling_rate': sampling_rate}))
+        SVstreams.append(Stream(SVstream))
+    return SVstreams
