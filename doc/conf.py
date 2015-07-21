15e19c8ed0b755b212650e384320c99fe86830cd
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2015-07-21 04:27:36 +0000

    Doc update

--- doc/conf.py
+++ doc/conf.py
@@ -137,7 +137,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'EQcorrscan'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
