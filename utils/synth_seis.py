ced620cc27cf450b3f75d3901fca3f0f697f8745
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2015-10-11 00:01:53 +0000

    debug
    Former-commit-id: a47bab3012a488979d35ed570130f154148fba99 [formerly 64f13504c90cb46f1ed791cd94757465fb10a5e2]
    Former-commit-id: 95189feb0a4abfee50e39823cd47e308a570255f

--- utils/synth_seis.py
+++ utils/synth_seis.py
@@ -153,7 +153,7 @@ def template_grid(stations, nodes, travel_times, phase, PS_ratio=1.68, \
             # Check that the template length is long enough to include the SP
             if SP_time*samp_rate > flength-11:
                 #warnings.warn('Cannot make this template, SP-time '+str(SP_time)+\
-                                ' longer than length: '+str(flength/samp_rate))
+                                # ' longer than length: '+str(flength/samp_rate))
                 continue
             else:
                 tr.data=seis_sim(SP=int(SP_time*samp_rate), amp_ratio=1.5,\
