20e5532946c679dbe8af8658b9a43a4c5c2e4638
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2015-10-20 20:59:27 +0000

    update test

--- eqcorrscan/scripts/synth_test.py
+++ eqcorrscan/scripts/synth_test.py
@@ -3,3 +3,20 @@ Script to validate the synthetic detector method against test earthquakes
 """
 
 def synth_from_sfile(sfile):
+    """
+    Function to generate a synthetic template for a given s-file
+
+    :type path: str
+    :param path: Path to the sfile
+    """
+    from eqcorrscan.utils import Sfile_util
+    from eqcorrscan.utils import syth_seis
+    picks=Sfile_util.readpicks(sfile)
+    # We only want P and S phases
+    picks=[p for p in picks if p.phase in ['P','S']]
+    # We want a list of the stations that we have picks for
+    stations=list(set([p.station for p in picks]))
+    # Loop through the stations
+    synths=[]
+    for station in stations:
+        
