08654b05f92bbf9ec9afa3c791b0a0080430f9ff
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2016-03-07 23:17:25 +0000

    Cope with sac IO support changes
    Former-commit-id: b5adce4c004f89ec63bd03975f064806b0e10d02 [formerly 7bbaad2479e5a7c598baa5677a344b8376698e39]
    Former-commit-id: ae4ae1964ec81d74ba86ef319e42d29a1f45b999

--- eqcorrscan/utils/sac_util.py
+++ eqcorrscan/utils/sac_util.py
@@ -4,6 +4,10 @@ objects.
 
 Authors: Calum Chamberlain and the EQcorrscan developers.
 
+.. note:: This functionality is not supported for obspy versions below \
+    1.0.0 as references times are not read in by SACIO, which are needed \
+    for defining pick times.
+
 This file is part of EQcorrscan.
 
     EQcorrscan is free software: you can redistribute it and/or modify
@@ -26,6 +30,13 @@ from __future__ import unicode_literals
 import warnings
 
 
+import obspy
+if int(obspy.__version__.split('.')[0]) < 1:
+    msg = 'Obspy version: ' + obspy.__version__ + ' does not have correct ' +\
+        'reference time handling, please upgrade to version > 1.0.0'
+    raise NotImplementedError(msg)
+
+
 def sactoevent(st, debug=0):
     """
     Function to convert SAC headers to obspy event class.
@@ -36,10 +47,19 @@ def sactoevent(st, debug=0):
     :pram debug: Debug level, larger number = more output.
 
     :returns: obspy.core.Event
+
+    .. note:: This functionality is not supported for obspy versions below \
+        1.0.0 as references times are not read in by SACIO, which are needed \
+        for defining pick times.
     """
     from obspy.core.event import Event, Origin, WaveformStreamID, Pick
     from obspy import Stream, UTCDateTime
     import numpy as np
+
+    # Set the default SAC nan values
+    float_nan = -12345.0
+    int_nan = -12345
+
     if not isinstance(st, Stream):
         msg = 'st must be a stream object, if you have just read in ' +\
             'multiple SAC files you may have a list of streams, convert ' +\
@@ -55,17 +75,25 @@ def sactoevent(st, debug=0):
     # Now we need to create an event!
     event = Event()
     event.origins.append(Origin())
-    event.origins[0].time = UTCDateTime(year=st[0].stats.sac['nzyear'],
-                                        julday=st[0].stats.sac['nzjday'],
-                                        hour=st[0].stats.sac['nzhour'],
-                                        minute=st[0].stats.sac['nzmin'],
-                                        second=st[0].stats.sac['nzsec'],
+    print(st[0].stats.sac.keys())
+    event.origins[0].time = UTCDateTime(year=st[0].stats.sac.nzyear,
+                                        julday=st[0].stats.sac.nzjday,
+                                        hour=st[0].stats.sac.nzhour,
+                                        minute=st[0].stats.sac.nzmin,
+                                        second=st[0].stats.sac.nzsec,
                                         microsecond=st[0].stats.
-                                        sac['nzmsec'] * 1000)
+                                        sac.nzmsec * 1000)
     try:
         event.origins[0].latitude = st[0].stats.sac.evla
         event.origins[0].longitude = st[0].stats.sac.evlo
         event.origins[0].depth = st[0].stats.sac.evdp
+        # Catch filled with 12345.0 as nan
+        if event.origins[0].latitude == float_nan:
+            event.origins[0].latitude = np.nan
+        if event.origins[0].longitude == float_nan:
+            event.origins[0].longitude = np.nan
+        if event.origins[0].depth == float_nan:
+            event.origins[0].depth = np.nan
     except KeyError:
         event.origins[0].latitude = np.nan
         event.origins[0].longitude = np.nan
@@ -84,6 +112,15 @@ def sactoevent(st, debug=0):
             pick_key = 't' + str(pick_number)
             phase_key = 'kt' + str(pick_number)
             try:
+                if tr.stats.sac[pick_key] == float_nan:
+                    # in version 0.10.2 and before. rather than not include
+                    # the keys, the variables are filled with SAC nans.
+                    if debug > 1:
+                        msg = 'No pick in position ' + pick_key + \
+                            ' for trace: ' + tr.stats.station + '.' + \
+                            tr.stats.channel
+                        warnings.warn(msg)
+                    continue
                 pick_time = reference_time + tr.stats.sac[pick_key]
                 phase_hint = tr.stats.sac[phase_key].split()[0]
             except KeyError:
