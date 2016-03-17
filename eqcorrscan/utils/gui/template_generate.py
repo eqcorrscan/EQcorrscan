67013b64a50577db81adb7e2f3717ac6c20c7fbc
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2016-03-18 08:14:44 +1300

    Add info for installation of GUI dependencies
    Former-commit-id: ed77908fc32c75f2a12c2ddce6b77f46d59ab275 [formerly 7d0e4b3eda44f43379f0b94a17aa94d968cd8adc]
    Former-commit-id: cf1943b935f82aaaf76923c96795c285a8f82bc5

---
+++ eqcorrscan/utils/gui/template_generate.py
@@ -0,0 +1,14 @@
+"""
+Python QT GUI for selection of template events.  In early stages of \
+development, but the plan is to have a catalogue plot window with button \
+options including:\
+- cluster
+- stack
+- plot waveforms
+- generate templates
+It should also have a list of events which can be selected from for all of \
+those actions and highlighted in the catalogue plot window.  This will end up \
+being a dog to write I think.  Any help would be appreciated!
+
+Part of EQcorrscan, copyright the authors 2016, 2015. Licencec under the LGPL.
+"""
