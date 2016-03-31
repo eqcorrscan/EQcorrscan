a64eedb260d22d3a1584f4984d548f33bc286d89
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2016-04-01 11:19:00 +1300

    Update utils doc strings
    Former-commit-id: 909ec432dc700fece9298753f7810a2d3c1bef64

--- eqcorrscan/utils/locate.py
+++ eqcorrscan/utils/locate.py
@@ -1,23 +1,13 @@
 r"""Functions to locate earthquakes detected by EQcorrscan.
 Designed primarily locate stacks of detections to give family locations.
-Extensions may later be written.
+Extensions may later be written, not tested for accuracy.
 
-Copyright 2015 Calum Chamberlain
+:copyright:
+    Calum Chamberlain, Chet Hopp.
 
-This file is part of EQcorrscan.
-
-    EQcorrscan is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    EQcorrscan is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with EQcorrscan.  If not, see <http://www.gnu.org/licenses/>.
+:license:
+    GNU Lesser General Public License, Version 3
+    (https://www.gnu.org/copyleft/lesser.html)
 """
 from __future__ import absolute_import
 from __future__ import division
