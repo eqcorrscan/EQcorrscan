23561e4363e94ef3b496815ff4b316961ba8c9fc
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2016-05-13 00:28:50 +0000

    Update conf for rtd

--- eqcorrscan/doc/conf.py
+++ eqcorrscan/doc/conf.py
@@ -145,7 +145,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not READ_THE_DOCS:
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
