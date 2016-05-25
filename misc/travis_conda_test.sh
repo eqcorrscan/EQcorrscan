1e97a9832fe68e24959875bf53a8947aec7b1752
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2016-05-25 01:22:38 +0000

    Attempt forced conda setup

--- misc/travis_conda_test.sh
+++ misc/travis_conda_test.sh
@@ -11,9 +11,14 @@ source ~/virtualenv/python3.5/bin/activate
 python --version
 pip --version
 
-export OS="Linux"; export py="3.5"
+export OS="Linux"; export py=$1
+echo $py
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-${OS}-x86_64.sh -O miniconda.sh
+if [[ "${py:0:1}" == '2' ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda-latest-${OS}-x86_64.sh -O miniconda.sh;
+else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-${OS}-x86_64.sh -O miniconda.sh
+fi
 bash miniconda.sh -b -p $HOME/miniconda
 
 export PATH="$HOME/miniconda/bin:$PATH"
@@ -23,6 +28,12 @@ conda update -q conda
 
 conda info -a
 
+if [[ "${py:0:1}" == '3' ]]; then
+  OPENCV="-c menpo opencv3=3.1.0"
+else
+  OPENCV="opencv"
+fi
+
 if [[ "$py" == "3.3" ]]; then
     NUMPY_VERSION=1.9.2
     SCIPY_VERSION=0.16.0
@@ -31,43 +42,42 @@ if [[ "$py" == "3.3" ]]; then
     PYPROJ=""
     PYTEST="pytest>=2.8"
     PYFLAKES="pyflakes=0.9.0"
-elif [[ "$py" == "3.5" ]]; then
-    NUMPY_VERSION=1.9.2
-    SCIPY_VERSION=0.16.0
-    MPL_VERSION=1.4.3
-    BASEMAP_VERSION=1.0.7
-    PYPROJ=""
-    PYTEST="pytest>=2.8"
-    PYFLAKES="pyflakes=1.0.0"
-elif [[ "${py:0:1}" == '3' ]]; then
-    NUMPY_VERSION=1.10.4
-    SCIPY_VERSION=0.17.0
-    MPL_VERSION=1.5.1
-    BASEMAP_VERSION=1.0.7
-    PYPROJ="pyproj"
-    PYTEST=""
-    PYFLAKES="pyflakes=0.9.0"
+    conda create -n test-environment python=$py numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION matplotlib=$MPL_VERSION basemap=$BASEMAP_VERSION $PYPROJ flake8 future lxml decorator sqlalchemy mock nose gdal docopt coverage requests
+    source activate test-environment
+    conda install $PYFLAKES
+    conda install $OPENCV
+elif [[ "${py:0:1}" == "3" ]]; then
+    conda config --add channels menpo
+    conda create -n test-environment --file misc/py35_test_env.lis
+    source activate test-environment
+#elif [[ "${py:0:1}" == '3' ]]; then
+#    NUMPY_VERSION=1.10.4
+#    SCIPY_VERSION=0.17.0
+#    MPL_VERSION=1.5.1
+#    BASEMAP_VERSION=1.0.7
+#    PYPROJ="pyproj"
+#    PYTEST=""
+#    PYFLAKES="pyflakes=0.9.0"
+#    conda create -n test-environment python=$py numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION matplotlib=$MPL_VERSION basemap=$BASEMAP_VERSION $PYPROJ flake8 future lxml decorator sqlalchemy mock nose gdal docopt coverage requests
+#    source activate test-environment
+#    conda install $PYFLAKES
+#    conda install $OPENCV
 else
-    NUMPY_VERSION=1.10.4
-    SCIPY_VERSION=0.17.0
-    MPL_VERSION=1.5.1
-    BASEMAP_VERSION=1.0.7
-    PYPROJ="pyproj"
-    PYTEST=""
-    PYFLAKES="pyflakes=0.9.0"
+    conda create -n test-environment --file misc/py2_test_env.lis
+    source activate test-environment
+#    NUMPY_VERSION=1.10.4
+#    SCIPY_VERSION=0.17.0
+#    MPL_VERSION=1.5.1
+#    BASEMAP_VERSION=1.0.7
+#    PYPROJ="pyproj"
+#    PYTEST=""
+#    PYFLAKES="pyflakes=0.9.0"
+#    conda create -n test-environment python=$py numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION matplotlib=$MPL_VERSION basemap=$BASEMAP_VERSION $PYPROJ flake8 future lxml decorator sqlalchemy mock nose gdal docopt coverage requests
+#    source activate test-environment
+#    conda install $PYFLAKES
+#    conda install $OPENCV
 fi
 
-if [[ "${py:0:1}" == '3' ]]; then
-  OPENCV="-c menpo opencv3=3.1.0"
-else
-  OPENCV="opencv"
-fi
-
-conda create -n test-environment python=$py numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION matplotlib=$MPL_VERSION basemap=$BASEMAP_VERSION $PYPROJ flake8 future lxml decorator sqlalchemy mock nose gdal docopt coverage requests
-source activate test-environment
-
-conda install $PYFLAKES
-conda install $OPENCV
 pip install coveralls
 pip install geographiclib
 pip install https://github.com/megies/PyImgur/archive/py3.zip
