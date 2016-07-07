b187895b88ff2f6fea43885e77ef48178c5be502
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2016-07-08 11:45:25 +1200

    Force xdist for tests

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ if sys.version_info.major == 2:
         install_requires = ['numpy>=1.8.0', 'obspy>=1.0.0',
                             'matplotlib>=1.3.0', 'joblib>=0.8.4',
                             'scipy>=0.14', 'multiprocessing',
-                            'LatLon']
+                            'LatLon', 'apipkg']
     else:
         install_requires = ['numpy>=1.8.0', 'obspy>=1.0.0',
                             'matplotlib>=1.3.0', 'joblib>=0.8.4',
@@ -72,7 +72,7 @@ else:
     if not READ_THE_DOCS:
         install_requires = ['numpy>=1.8.0', 'obspy>=0.10.2',
                             'matplotlib>=1.3.0', 'joblib>=0.8.4',
-                            'scipy>=0.14', 'LatLon']
+                            'scipy>=0.14', 'LatLon', 'apipkg']
     else:
         install_requires = ['numpy>=1.8.0', 'obspy>=0.10.2',
                             'matplotlib>=1.3.0', 'joblib>=0.8.4',
