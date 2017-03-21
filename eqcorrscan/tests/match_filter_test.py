b353bdbc8e5dd79ea8b8476f313ff0d7a669124d
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2017-03-21 14:49:37 +1300

    Update match_filter_test.py

--- eqcorrscan/tests/match_filter_test.py
+++ eqcorrscan/tests/match_filter_test.py
@@ -133,14 +133,14 @@ class TestCoreMethods(unittest.TestCase):
         """Send it the wrong type."""
         ccc = normxcorr2(template=[0, 1, 2, 3, 4], image='bob')
         self.assertEqual(ccc, 'NaN')
-    
+
     def test_spike_test(self):
         """Check that an error is raised!"""
         stream = read()
         stream[0].data[100] = 1e20
         with self.assertRaises(MatchFilterError):
             _spike_test(stream)
-            
+
 
 class TestSynthData(unittest.TestCase):
     def test_debug_range(self):
