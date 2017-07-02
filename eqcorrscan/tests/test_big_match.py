ac7e533ca49262576d3055965526144360e3d799
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2017-07-02 21:48:14 +0000

    Add test for large test-case

--- eqcorrscan/tests/test_big_match.py
+++ eqcorrscan/tests/test_big_match.py
@@ -6,59 +6,29 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import copy
-import os
 import unittest
-import warnings
 
-from obspy.clients.fdsn import Client
-from obspy import UTCDateTime
-from obspy.core.event import Pick
-
-from eqcorrscan.tutorials.get_geonet_events import get_geonet_events
-from eqcorrscan.utils import catalog_utils
-from eqcorrscan.core import template_gen
-from eqcorrscan.utils import pre_processing
+from eqcorrscan.utils import synth_seis
+from eqcorrscan.utils.timer import Timer
+from eqcorrscan.core.match_filter import match_filter
 
 
-class TestGeoNetCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        client = Client('GEONET')
-        t1 = UTCDateTime(2016, 9, 4)
-        t2 = t1 + (86400 * 20)
-        catalog = get_geonet_events(
-            startdate=t1, enddate=t2, minmag=4, minlat=-49, maxlat=-35,
-            minlon=175.0, maxlon=185.0)
-        catalog = catalog_utils.filter_picks(
-            catalog, channels=['EHZ'], top_n_picks=5)
-        for event in catalog:
-            extra_pick = Pick()
-            extra_pick.phase_hint = 'S'
-            extra_pick.time = event.picks[0].time + 10
-            extra_pick.waveform_id = event.picks[0].waveform_id
-            event.picks.append(extra_pick)
-        cls.templates = template_gen.from_client(
-            catalog=catalog, client_id='GEONET', lowcut=2.0, highcut=9.0,
-            samp_rate=50.0, filt_order=4, length=3.0, prepick=0.15, swin='all',
-            process_len=3600)
-        # Download and process the day-long data
-        bulk_info = [(tr.stats.network, tr.stats.station, '*',
-                      tr.stats.channel, t1 + (4 * 3600), t1 + (5 * 3600))
-                     for tr in cls.templates[0]]
-        # Just downloading an hour of data
-        print('Downloading data')
-        st = client.get_waveforms_bulk(bulk_info)
-        st.merge(fill_value='interpolate')
-        st.trim(t1 + (4 * 3600), t1 + (5 * 3600)).sort()
-        # This is slow?
-        print('Processing continuous data')
-        cls.st = pre_processing.shortproc(
-            st, lowcut=2.0, highcut=9.0, filt_order=4, samp_rate=50.0,
-            debug=0, num_cores=1)
-        cls.st.trim(t1 + (4 * 3600), t1 + (5 * 3600)).sort()
-        cls.template_names = [str(template[0].stats.starttime)
-                              for template in cls.templates]
+class TestSynthLarge(unittest.TestCase):
+    templates, data, seeds = synth_seis.generate_synth_data(
+        nsta=10, ntemplates=10, nseeds=5, samp_rate=100, t_length=6,
+        max_amp=5, max_lag=5, debug=0)
+    with Timer() as t:
+        detections = match_filter(
+            template_names=[str(i) for i in range(len(templates))],
+            template_list=templates, st=data, threshold=8, threshold_type='MAD',
+            trig_int=6, plotvar=False, cores=4, output_event=False)
+    print('Parallel run took %f seconds' % t.secs)
+    with Timer() as t:
+        detections = match_filter(
+            template_names=[str(i) for i in range(len(templates))],
+            template_list=templates, st=data, threshold=8, threshold_type='MAD',
+            trig_int=6, plotvar=False, cores=None, output_event=False)
+    print('Serial run took %f seconds' % t.secs)
 
 
 if __name__ == '__main__':
