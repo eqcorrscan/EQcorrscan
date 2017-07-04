2c9d2a41c4cceeb53095fefa8ca81af76573ec18
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2017-07-05 07:49:34 +1200

    Add possible faster routine
    Doesn't work yet.

--- eqcorrscan/utils/src/norm.c
+++ eqcorrscan/utils/src/norm.c
@@ -21,6 +21,68 @@
 #include <stdlib.h>
 #include <math.h>
 
+int multi_normalise_2(float *ccc, int ccc_len, float *image, float norm_sum, int template_len, int n)
+{
+	int i, j, k;
+	float mean, std, sum=0.0, var=0.0;
+
+    // Compute starting mean, will update this
+	for (i=0; i < template_len; ++i)
+	{
+		sum += image[i];
+	}
+	mean = sum / template_len;
+
+    // Compute starting standard deviation, will update this
+	for (i=0; i < template_len; ++i)
+	{
+		var += powf(image[i] - mean, 2);
+	}
+	std = sqrtf(var / (template_len));
+    if (std == 0.0)
+    {
+        for (k=0; k<n; ++k)
+        {
+            ccc[k] = 0.0;
+        }
+    }
+    else
+    {
+        for (k=0; k<n; ++k)
+        {
+        	ccc[k] = (ccc[k] - norm_sum * mean ) / std;
+        }
+    }
+    // Loop through updating as we go
+	for(i=1; i<ccc_len; ++i)
+	{
+	    mean = mean + (image[i + template_len - 1] - image[i - 1]) / template_len;
+	    // Don't know of a usefully accurate and efficient method :(
+	    var = 0.0;
+	    for (j=0; j<template_len; ++j)
+	    {
+	        var += powf(image[i + j] - mean, 2);
+	    }
+		std = sqrtf(var / template_len);
+		if (std == 0.0)
+		{
+		    for (k=0; k<n; ++k)
+		    {
+		        ccc[(i * n) + k] = 0.0;
+		    }
+		}
+		else
+		{
+		    // Normalize by the std and mean
+		    for (k=0; k<n; ++k)
+		    {
+		        ccc[(i * n) + k] = (ccc[(i * n) + k] - norm_sum * mean ) / std;
+		    }
+    	}
+	}
+	return 0;
+}
+
 int normalise(float *ccc, int ccc_len, float *image, float norm_sum, int template_len)
 {
 	int i, j;
