768e760273eb0018c5277da98726287e7e201ca2
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2017-07-13 09:48:30 +1200

    Remove issue-rich pytest arg config

--- conftest.py
+++ conftest.py
@@ -1,16 +1,3 @@
-# Over-ride the -n auto in travis as this goes of the wall
-import sys
-import os
-
-
-def pytest_cmdline_preparse(args):
-    is_travis = 'TRAVIS' in os.environ
-    if 'xdist' in sys.modules and is_travis:
-        if "-n" in args:
-            args[args.index("-n") + 1] = "2"
-        else:
-            args[:] = args + ["-n", "2"]
-    print(args)
 
 
 def pytest_addoption(parser):
