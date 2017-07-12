d8f7077d5ecd0ad958651dcd4668f715d3b8012f
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2017-07-13 10:30:38 +1200

    Can't get auto working on travis

--- conftest.py
+++ conftest.py
@@ -1,3 +1,16 @@
+# Over-ride the -n auto in travis as this goes of the wall
+# import sys
+# import os
+#
+#
+# def pytest_cmdline_preparse(config, args):
+#     is_travis = 'TRAVIS' in os.environ
+#     if 'xdist' in sys.modules and not is_travis:
+#         if "-n" in args:
+#             args[args.index("-n") + 1] = "2"
+#         else:
+#             args[:] = args + ["-n2"]
+#     print(args)
 
 
 def pytest_addoption(parser):
