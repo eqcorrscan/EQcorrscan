fbf329152783429d4770e1e7e0701fe25895d24e
Chris Scott <chris.scott@nesi.org.nz>
2017-08-29 10:40:31 +1200

    Trying to get isnanf on windows

--- eqcorrscan/lib/multi_corr.c
+++ eqcorrscan/lib/multi_corr.c
@@ -22,6 +22,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#if (defined(_MSC_VER) && _MSC_VER < 1800)
+    #include <float.h>
+    #define isnanf(x) _isnanf(x)
+#endif
 #include <fftw3.h>
 #if defined(__linux__) || defined(__linux) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
     #include <omp.h>
