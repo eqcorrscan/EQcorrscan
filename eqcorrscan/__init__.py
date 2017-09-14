25f8b890a32422c989a54f674f7fc6ef82cb562f
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2017-09-14 14:16:09 +1200

    Bump version number

--- eqcorrscan/__init__.py
+++ eqcorrscan/__init__.py
@@ -13,7 +13,7 @@ import warnings
 
 __all__ = ['core', 'utils', 'tutorials']
 
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 # Cope with changes to name-space to remove most of the camel-case
 _import_map = {
