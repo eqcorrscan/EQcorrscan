8618d2fdc8e67a4dedbd94fa2d6c506a63399224
FelixHa <felix.halpaap@uib.no>
2017-11-11 02:16:58 +0100

    Fixed bug with 2-letter vs 3-letter channel code comparison
    Added additional (ugly) lines to compare only first and last letter of 2 or 3-letter channel codes.

--- eqcorrscan/core/template_gen.py
+++ eqcorrscan/core/template_gen.py
@@ -1097,7 +1097,8 @@ def template_gen(picks, st, length, swin='all', prepick=0.05,
             print(tr)
             st.remove(tr)
         else:
-            st_stachans.append('.'.join([tr.stats.station, tr.stats.channel]))
+            st_stachans.append('.'.join([tr.stats.station, 
+                        ''.join([tr.stats.channel[0], tr.stats.channel[-1]])]))
     for i, station in enumerate(stations):
         if '.'.join([station, channels[i]]) not in st_stachans and debug > 0:
             warnings.warn('No data provided for ' + station + '.' +
@@ -1127,8 +1128,9 @@ def template_gen(picks, st, length, swin='all', prepick=0.05,
                         starttime = pick.time - prepick
                 else:
                     if pick.waveform_id.station_code == tr.stats.station and \
-                            pick.waveform_id.channel_code ==\
-                            tr.stats.channel:
+                            ''.join([pick.waveform_id.channel_code[0],\
+                                     pick.waveform_id.channel_code[-1]]) ==\
+                            ''.join([tr.stats.channel[0],tr.stats.channel[-1]]):
                         starttime = pick.time - prepick
                     # Cope with taking all the horizontals for S-picks.
                     elif all_horiz and pick.waveform_id.station_code ==\
