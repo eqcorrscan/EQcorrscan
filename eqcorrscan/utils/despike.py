39ea52bea4c37b88efd9fd70dbbb5aaa152e5657
flixha <fha053@uib.no>
2018-03-01 10:40:06 +0100

    Stickler code beautifications

--- eqcorrscan/utils/despike.py
+++ eqcorrscan/utils/despike.py
@@ -55,10 +55,10 @@ def median_filter(tr, multiplier=10, windowlength=0.5,
     num_cores = cpu_count()
     if debug >= 1:
         data_in = tr.copy()
-    ## Note - might be worth finding spikes in filtered data
+    # Note - might be worth finding spikes in filtered data
     filt = tr.copy()
     filt.detrend('linear')
-    #filt.filter('bandpass', freqmin=9.999,
+    # filt.filter('bandpass', freqmin=9.999,
     #            freqmax=(tr.stats.sampling_rate / 2) - 1)
     data = filt.data
     del(filt)
