5fba328b52f87c8ef6f66ec6cc7152bbe41e5634
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2018-06-23 16:51:13 +1200

    Remove logging changes

--- CHANGES.md
+++ CHANGES.md
@@ -9,7 +9,6 @@
   is the same value repeated (#224, #230).
 * BUG-FIX: template generation `from_client` methods for swin=P_all or S_all
   now download all channels and return them (as they should). See #235 and #206
-* Change standard debug printing to logging (#229).
 * Change from raising an error if data from a station are not long enough, to
   logging a critical warning and not using the station.
 * Add ability to give multiple `swin` options as a list. Remains backwards
--- eqcorrscan/utils/debug_log.py
+++ eqcorrscan/utils/debug_log.py
@@ -14,13 +14,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import logging
 
-LOG = {0: logging.debug, 1: logging.info, 2: logging.warning, 3: logging.error,
-       4: logging.critical}
-
-
-def debug_print(string, debug_level, print_level, use_logging=True):
+def debug_print(string, debug_level, print_level):
     """
     Print the string if the print_level exceeds the debug_level.
 
@@ -30,21 +25,14 @@ def debug_print(string, debug_level, print_level, use_logging=True):
     :param print_level: Print-level for statement
     :type debug_level: int
     :param debug_level: Output level for function
-    :type use_logging: bool
-    :param use_logging: Uses logging to output information
 
     .. rubric:: Example
-    >>> debug_print("Albert", 2, 0, use_logging=False)
-    >>> debug_print("Norman", 0, 2, use_logging=False)
+    >>> debug_print("Albert", 2, 0)
+    >>> debug_print("Norman", 0, 2)
     Norman
     """
-    if not use_logging and print_level > debug_level:
+    if print_level > debug_level:
         print(string)
-    if use_logging:
-        try:
-            LOG[debug_level](string)
-        except IndexError:
-            LOG[4](string)
 
 
 if __name__ == '__main__':
