b2d5a0da3dc0fe4abb805ee06d9825316771af8e
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2018-08-21 02:14:13 +0000

    Fix test script

--- eqcorrscan/scripts/test_eqcorrscan.py
+++ eqcorrscan/scripts/test_eqcorrscan.py
@@ -30,7 +30,8 @@ Logger = logging.getLogger(__name__)
 
 VERSION = eqcorrscan.__version__
 TEST_PATH = os.path.dirname(tests.__file__)
-PKG_PATH = os.path.dirname(os.path.dirname(eqcorrscan.__file__))
+PKG_PATH = os.path.dirname((eqcorrscan.__file__))
+WORKING_DIR = os.path.join(os.path.expanduser("~"), ".eqcorrscan")
 TAG_URL = "https://github.com/eqcorrscan/EQcorrscan/archive/{0}.zip".format(
     VERSION)
 TEST_DATA_PATH = os.path.join(TEST_PATH, 'test_data')
@@ -56,12 +57,39 @@ class cd:
 
 def download_test_data():
     """Check if test data are installed, and if not, download them"""
+    test_data_downloaded = False
+    control_files = [
+        {"name": "pytest.ini", "downloaded": False},
+        {"name": "conftest.py", "downloaded": False},
+        {"name": ".coveragerc", "downloaded": False}]
+    pytest_ini_downloaded = False
+    conftest_downloaded = False
+    coveragerc_downloaded = False
     if os.path.isdir(TEST_DATA_PATH):
-        if len(glob.glob(TEST_DATA_PATH)) > 0:
+        if len(glob.glob(os.path.join(TEST_DATA_PATH, "*"))) > 0:
             Logger.info("Found test data at: {0}".format(TEST_DATA_PATH))
-            return
+            test_data_downloaded = True
+        if os.path.isdir(WORKING_DIR):
+            for control_file in control_files:
+                working_path = os.path.join(WORKING_DIR, control_file["name"])
+                if os.path.isfile(working_path):
+                    Logger.info("{0} already here: {1}".format(
+                        control_file["name"], working_path))
+                    control_file["downloaded"] = True
+    else:
+        os.makedirs(TEST_DATA_PATH)
+    if not os.path.isdir(WORKING_DIR):
+        os.makedirs(WORKING_DIR)
+    if not os.path.islink(os.path.join(WORKING_DIR, "eqcorrscan")):
+        # Make a symbolic link so that test data can be found
+        Logger.debug("Making symlink to install")
+        os.symlink(PKG_PATH, os.path.join(WORKING_DIR, "eqcorrscan"))
+
+    control_files_downloaded = all([c["downloaded"] for c in control_files])
+    if test_data_downloaded and control_files_downloaded:
+        return
+
     Logger.info("Downloading test data from github")
-    os.makedirs(TEST_DATA_PATH)
     with cd(TEST_DATA_PATH):
         # Get the whole zip for this release
         Logger.info("Downloading from {0}".format(TAG_URL))
@@ -71,22 +99,25 @@ def download_test_data():
         # Files that we want, test-data, conftest.py and pytest.ini
         test_data_path = "EQcorrscan-{0}/eqcorrscan/tests/test_data/".format(
             VERSION)
-        conftest = "EQcorrscan-{0}/conftest.py".format(VERSION)
-        contents.extract(conftest, '.')
-        pytestini = "EQcorrscan-{0}/pytest.ini".format(VERSION)
-        contents.extract(pytestini, '.')
-        for file in contents.namelist():
-            if file.startswith(test_data_path):
-                contents.extract(file, '.')
-        test_data = glob.glob(
-            "EQcorrscan-{0}/eqcorrscan/tests/test_data/*".format(VERSION))
-        for test_file in test_data:
-            Logger.debug("Moving {0}".format(test_file))
-            shutil.move(test_file, ".")
-        Logger.debug("Moving {0} to {1}".format(conftest, PKG_PATH))
-        shutil.move(conftest, PKG_PATH)
-        Logger.debug("Moving {0} to {1}".format(pytestini, PKG_PATH))
-        shutil.move(pytestini, PKG_PATH)
+
+        for control_file in control_files:
+            if not control_file["downloaded"]:
+                extract_path = "EQcorrscan-{0}/{1}".format(
+                    VERSION, control_file["name"])
+                contents.extract(extract_path, ".")
+                Logger.debug("Moving {0} to {1}".format(
+                    extract_path, WORKING_DIR))
+                shutil.move(extract_path, WORKING_DIR)
+
+        if not test_data_downloaded:
+            for file in contents.namelist():
+                if file.startswith(test_data_path):
+                    contents.extract(file, '.')
+            test_data = glob.glob(
+                "EQcorrscan-{0}/eqcorrscan/tests/test_data/*".format(VERSION))
+            for test_file in test_data:
+                Logger.debug("Moving {0}".format(test_file))
+                shutil.move(test_file, ".")
         shutil.rmtree("EQcorrscan-{0}".format(VERSION))
     return
 
@@ -98,7 +129,10 @@ def run_tests(arg_list):
     arg_list.extend(["--doctest-modules", "--runslow"])
     arg_list.extend(
         ["--ignore", "EGG-INFO", "--ignore", "eqcorrscan/utils/lib"])
-    with cd(PKG_PATH):
+    # arg_list.append(PKG_PATH)
+    with cd(WORKING_DIR):
+        Logger.info("Running tests from {0}".format(PKG_PATH))
+        Logger.info("pytest {0}".format(' '.join(arg_list)))
         pytest.main(args=arg_list)
 
 
