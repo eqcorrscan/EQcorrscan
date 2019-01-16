d05f8708a739fe48071281d71861eb2966d4c902
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2019-01-17 12:12:10 +1300

    Normalize data

--- eqcorrscan/utils/correlate.py
+++ eqcorrscan/utils/correlate.py
@@ -899,6 +899,9 @@ def _get_array_dicts(templates, stream, stack, copy_streams=True):
         t_ar = np.array(temps_with_seed).astype(np.float32)
         template_dict.update({seed_id: t_ar})
         stream_channel = stream.select(id=seed_id.split('_')[0])[0]
+        # Normalize data to endure no float overflow
+        stream_data = stream_channel.data / (np.max(
+            np.abs(stream_channel.data)) / 1e5))
         stream_dict.update(
             {seed_id: stream_channel.data.astype(np.float32)})
         stream_offset = int(
