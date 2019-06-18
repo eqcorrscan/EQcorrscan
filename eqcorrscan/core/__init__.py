d0082fdd6de25df445b8eef78fa959ae7d91bf53
calum-chamberlain <calum.chamberlain@vuw.ac.nz>
2019-06-18 12:44:30 +1200

    Remove depreciated functions

--- eqcorrscan/core/__init__.py
+++ eqcorrscan/core/__init__.py
@@ -11,8 +11,7 @@ import sys
 import importlib
 import warnings
 
-__all__ = ['template_gen', 'match_filter', 'bright_lights', 'lag_calc',
-           'subspace']
+__all__ = ['template_gen', 'match_filter', 'lag_calc', 'subspace']
 
 _import_map = {}
 
