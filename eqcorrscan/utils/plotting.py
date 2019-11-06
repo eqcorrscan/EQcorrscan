829887f97ba913c3c957d96c0f8523743980f5e3
eQ <45411706+Halauwet@users.noreply.github.com>
2019-11-06 13:57:55 +0700

    Update plotting.py
    Found an issue when construct template with option "plotdir" or "save_progress" on windows10, which can't use special character on the filename. So I added string format "strftime("%Y-%m-%dT%H%M%S")" for the default cccsum plot_name.

--- eqcorrscan/utils/plotting.py
+++ eqcorrscan/utils/plotting.py
@@ -2218,7 +2218,8 @@ def _match_filter_plot(stream, cccsum, template_names, rawthresh, plotdir,
     cccsum_plot = cccsum_plot[0:len(stream_plot.data)]
     cccsum_hist = cccsum_hist[0:len(stream_plot.data)]
     plot_name = "{0}/cccsum_plot_{1}_{2}.{3}".format(
-        plotdir, template_names[i], stream[0].stats.starttime, plot_format)
+        plotdir, template_names[i], stream[0].stats.starttime.strftime(
+                  "%Y-%m-%dT%H%M%S"), plot_format)
     plot_kwargs = dict(show=True)
     if plotdir is not None:
         if not os.path.isdir(plotdir):
