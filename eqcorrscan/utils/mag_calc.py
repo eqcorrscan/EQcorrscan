285ab76e7940ce9a944ca694fc06f2c3b7352dda
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2020-08-11 10:03:45 +1200

    Round to 1dp across all systems

--- eqcorrscan/utils/mag_calc.py
+++ eqcorrscan/utils/mag_calc.py
@@ -150,7 +150,7 @@ def calc_b_value(magnitudes, completeness, max_mag=None, plotvar=True):
     >>> # We can set a maximum magnitude:
     >>> b_values = calc_b_value(magnitudes, completeness=np.arange(3, 7, 0.2),
     ...                         plotvar=False, max_mag=5)
-    >>> round(b_values[4][1])
+    >>> round(b_values[4][1], 1)
     1.0
     """
     b_values = []
