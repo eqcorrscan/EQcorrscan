c359fde8b759622486d4acf00e383faa7cf9d496
flixha <felix.halpaap@uib.no>
2023-01-03 17:07:04 +0100

    fix reversion to set

--- eqcorrscan/core/match_filter/family.py
+++ eqcorrscan/core/match_filter/family.py
@@ -326,8 +326,8 @@ class Family(object):
         2
         """
         _detections = []
-        _detections = [_detections.append(d) for d in self.detections
-                       if not _detections.count(d)]
+        [_detections.append(d) for d in self.detections
+         if not _detections.count(d)]
         self.detections = _detections
         return self
 
