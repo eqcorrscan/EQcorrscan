ae55bb434154c4cbdb2f1fd67a3d9cf72f6c027c
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2024-06-29 08:30:37 +1200

    linter

--- eqcorrscan/core/template_gen.py
+++ eqcorrscan/core/template_gen.py
@@ -522,7 +522,8 @@ def _download_from_client(client, client_type, catalog, data_pad, process_len,
                     " {0}".format(pick))
                 continue
             if pick.waveform_id.channel_code is None:
-                Logger.error(f"Missing waveform_id.channel_code for Pick {pick}.")
+                Logger.error(
+                    f"Missing waveform_id.channel_code for Pick {pick}.")
                 raise TemplateGenError(
                     "Ensure that all picks have complete "
                     "waveform_id attributes")
